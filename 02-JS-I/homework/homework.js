4ce238bdc38580eeae8870b44782783d912f9d3b
lucasmedina <lucasmedina275@gmail.com>
2022-07-19 18:24:00 -0300

    septimo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -108,51 +108,61 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num / 2 === 0) {return true;
+  } else {return false;
+  }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num / 2 === 1) {return true;
+  } else {return false;
+  }
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return Math.pow (num,2) ;
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
-  
+  return Math.pow (num,3) ;
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+  return Math.pow(num,exponent) ;
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  var resto = num * 0.1 ;
+  var entero = num - resto ;
+  if (resto >= 0.50) {return entero +1;
+  } else { return entero;
+  } 
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  var resto = num * 0.10;
+  var entero = num - resto;
+  return entero + 1;
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return + 
 }
 
 function esPositivo(numero) {
